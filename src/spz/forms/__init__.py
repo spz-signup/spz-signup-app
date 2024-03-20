c31ffef6a4721035fa7bda5477d1b11ef20d6133
urvdp <urvdp@student.kit.edu>
2024-03-20 15:29:39 +0100

    finish implementation of the templates for setting the grade, ects and if the grade should be hidden

--- src/spz/forms/__init__.py
+++ src/spz/forms/__init__.py
@@ -1042,9 +1042,5 @@ def create_grade_form(applicants):
     return GradeForm
 
 
-class GradeViewForm(FlaskForm):
-    pass
-
-
 class AttendanceForm(FlaskForm):
     attendance = HiddenField("attendance_id")
